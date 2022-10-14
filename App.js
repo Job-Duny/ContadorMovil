8c002b1513ae90af138b1f9af21d2b01c05f217e
Job-Duny <gpinam2000@alumno.ipn.mx>
2022-10-14 09:27:31 -0500

    dd

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+  const add= () => setCounter(c => c+1)
+  const res = () => setCounter( c => c-1)
+
   return (
     <View style={styles.container}>
-      <View style={styles.sbConti}>
 
+      <Text style={styles.counter}>{ counter }</Text>
+      <View style={styles.subContainer}>
+        <TouchableOpacity style={styles.btn} onPress={add}>
+          <Text style={styles.text}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={res}>
+          <Text style={styles.text}>-</Text>
+        </TouchableOpacity>
       </View>
+      
     </View>
   );
 }
@@ -18,7 +32,34 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  sbConti: {
-    
-  }
+  subContainer:{
+    justifyContent: 'center',
+    height: 50,
+    width: '100%',
+    paddingHorizontal:10,
+    flexDirection: 'row'
+
+  },
+  btn:{
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor:'#5B89A6',
+  },
+  text:{
+    fontSize:40,
+    color: 100,
+    paddingHorizontal:10,
+  },
+
+  count:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+
+  counter:{
+    fontSize: 130,
+  },
 });
